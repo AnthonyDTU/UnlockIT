7086696a0a5f142ada47c81781cd9ed591f84d37
AnthonyDTU <71980488+AnthonyDTU@users.noreply.github.com>
2023-02-23 18:09:30 +0100

    Created an NFC Simulator

--- UnlockIT/UnlockIT/views/HomeView.swift
+++ UnlockIT/UnlockIT/views/HomeView.swift
@@ -10,38 +10,47 @@ import SwiftUI
 struct HomeView: View {
     
     @EnvironmentObject var user : User
-    @ObservedObject var name : User
     
     @State private var showLoginView : Bool = false
+    @State private var scanNow : Bool = false
+    @State private var selectedLevel : Int = 0
     
     var scanLockTapGesture: some Gesture {
         TapGesture()
             .onEnded {
                 print("Scanning Lock")
+                scanNow = true
             }
     }
     
     
     var body: some View {
     
-        VStack {
-                        
-            HStack {
-                Spacer(minLength: 20)
-                Image("UnlockItLogo.png")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(25)
-                    .gesture(scanLockTapGesture)
-                Spacer(minLength: 20)
+        
+        ZStack {
+            VStack {
+                            
+                HStack {
+                    Spacer(minLength: 20)
+                    Image("UnlockItLogo.png")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(25)
+                        .gesture(scanLockTapGesture)
+                    Spacer(minLength: 20)
+                }
+                
+                if user.isAdmin {
+                    AdminControls()
+                }
             }
             
             
-            
-            if user.isAdmin {
-                AdminControls()
+            if scanNow {
+                NFCSimulator(scanNow: $scanNow, selectedLevel: $selectedLevel)
             }
         }
+       
         
     }
 }
---
+++ UnlockIT/UnlockIT/views/NFCSimulator.swift
@@ -0,0 +1,100 @@
+//
+//  NFCSimulator.swift
+//  UnlockIT
+//
+//  Created by Anton Lage on 23/02/2023.
+//
+
+import SwiftUI
+
+struct NFCSimulator: View {
+    @Binding var scanNow : Bool
+    @Binding var selectedLevel : Int
+    
+    /*
+    var dragToRemoveGesture : DragGesture {
+        DragGesture()
+            .onChanged{
+                
+            }
+            .onEnded{
+                
+            }
+    }
+    */
+    
+    var body: some View {
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .fill(.white)
+                .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 300, alignment: .bottom)
+            
+            VStack {
+                
+                Spacer()
+                Text("NFC Simulator")
+                    .font(Font.system(.largeTitle).bold())
+                    .foregroundColor(.gray)
+                Spacer()
+                Image(systemName: "iphone.gen2.radiowaves.left.and.right")
+                    .font(.system(size: 100.0))
+                    .foregroundColor(.blue)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        selectedLevel = 1
+                        scanNow = false
+                    } label: {
+                        Image(systemName: "1.circle.fill")
+                            .font(.system(size: 56.0))
+                            .foregroundColor(.blue)
+                    }
+                    
+                    Spacer()
+                    Button {
+                        selectedLevel = 2
+                        scanNow = false
+                    } label: {
+                        Image(systemName: "2.circle.fill")
+                            .font(.system(size: 56.0))
+                            .foregroundColor(.blue)
+                    }
+
+                    Spacer()
+                    Button {
+                        selectedLevel = 3
+                        scanNow = false
+                    } label: {
+                        Image(systemName: "3.circle.fill")
+                            .font(.system(size: 56.0))
+                            .foregroundColor(.blue)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                
+                Button("Cancel") {
+                    selectedLevel = 0
+                    scanNow = false
+                }
+                .frame(width: UIScreen.main.bounds.width - 100, height: 50)
+                .foregroundColor(.black)
+                .buttonStyle(.bordered)
+                .cornerRadius(15)
+                .tint(.gray)
+                
+                Spacer()
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width - 50, height: UIScreen.main.bounds.height - 300, alignment: .bottom)
+    
+    }
+}
+
+struct NFCSimulator_Previews: PreviewProvider {
+    static var previews: some View {
+        NFCSimulator(scanNow: .constant(true), selectedLevel: .constant(0))
+    }
+}
