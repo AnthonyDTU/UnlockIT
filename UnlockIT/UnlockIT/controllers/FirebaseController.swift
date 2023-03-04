52715417b3d8e8428d3c0b995a45d9714f78b3f7
AnthonyDTU <71980488+AnthonyDTU@users.noreply.github.com>
2023-03-04 18:39:25 +0100

    Started looking into firebase realtime database

--- UnlockIT/UnlockIT/controllers/FirebaseController.swift
+++ UnlockIT/UnlockIT/controllers/FirebaseController.swift
@@ -8,12 +8,13 @@
 import Foundation
 import Firebase
 
+
 class FirebaseController {
     
     func SignIn(user: User, email: String, password: String) async -> Bool {
         do {
             let authResults = try await Auth.auth().signIn(withEmail: email, password: password)
-            print(authResults.user.uid)
+            user.id = authResults.user.uid
             GetUserDataFromFirebase(user: user)
             return true
         }
@@ -25,6 +26,15 @@ class FirebaseController {
     
     func GetUserDataFromFirebase(user: User) {
         
+        var databaseRef = Database.database().reference()
+        databaseRef.child("Users/" + user.id).observeSingleEvent(of: .value, with: { snapshot in
+            if let data = snapshot.value as? [String:Any] {
+                user.name = data["username"] as! String
+                user.privilege = data["level"] as! Int
+                user.companyPosition = data["position"] as! String
+                user.department = data["department"] as! String
+            }
+        })
     }
     
     
